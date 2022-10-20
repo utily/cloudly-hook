bd956f0d3a78a9a236b62f072989f0e350db3014
Nils Hansander <nils.hansander@pax2pay.com>
2022-10-20 17:24:21 +0200

    Change instantiation of hooks and queue

--- Queue/index.ts
+++ Queue/index.ts
@@ -2,17 +2,19 @@ import * as storage from "cloudly-storage"
 import { Storage as QueueStorage } from "./Storage"
 
 export class Queue {
-	private constructor(private readonly client: storage.DurableObject.Client) {
-		this.client.onError = async (request, response) => {
-			console.log("error", request, response)
-			return false
-		}
+	#client: storage.DurableObject.Client
+	private constructor(private readonly namespace: storage.DurableObject.Namespace) {
+		// this.client.onError = async (request, response) => {
+		// 	console.log("error", request, response)
+		// 	return false
+		// }
 	}
 	async enqueue(hook: string, value: any): Promise<void> {
-		this.client.post<Record<string, any>>("/queue", { hook, value })
+		this.#client = this.namespace.open(hook)
+		this.#client.post<Record<string, any>>("/queue", { hook, value })
 	}
-	static open(client: storage.DurableObject.Client): Queue {
-		return new Queue(client)
+	static open(namespace: storage.DurableObject.Namespace): Queue {
+		return new Queue(namespace)
 	}
 }
 
