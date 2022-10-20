805aa95bed03bacba0271fe51d5073680772f5f8
Nils Hansander <nils.hansander@pax2pay.com>
2022-10-20 11:48:51 +0200

    Retry hook

--- Queue/Storage/Context.ts
+++ Queue/Storage/Context.ts
@@ -5,39 +5,42 @@ import * as platform from "../../platform"
 import { Item } from "../Item"
 
 export class Context {
-	private constructor(private readonly state: platform.DurableObjectState) {}
-	async enqueue(hook: Item): Promise<void> {
+	private constructor(private readonly state: platform.DurableObjectState, private readonly MAX_RETRIES = 5) {}
+	async enqueue(hook: Item, retries = 0, timeInSeconds = 10): Promise<void> {
 		let index = await this.state.storage.get<number>("index")
 		index = typeof index == "number" ? ++index : 0
-		console.log(index)
-		console.log("state: ", this.state.id)
-		await this.state.storage.put(`hook${index}`, hook)
+		console.log("inside enqueue", index)
+		await this.state.storage.put(`hook${index}`, { hook, retries })
 		await this.state.storage.put("index", index)
 		this.state.waitUntil(
 			this.state.storage.setAlarm(
-				isoly.DateTime.epoch(isoly.DateTime.nextSecond(isoly.DateTime.now(), 10), "milliseconds")
+				isoly.DateTime.epoch(isoly.DateTime.nextSecond(isoly.DateTime.now(), timeInSeconds), "milliseconds")
 			)
 		)
 	}
 	async dequeue(): Promise<Item | gracely.Error> {
 		let result: Item | gracely.Error
 		let index: number | undefined
-		let item: Item | undefined
+		let item: { hook: Item; retries: number } | undefined
 		if (
 			typeof (index = await this.state.storage.get<number>("index")) == "undefined" ||
-			!(item = await this.state.storage.get<Item>(`hook${index}`))
+			!(item = await this.state.storage.get<{ hook: Item; retries: number }>(`hook${index}`))
 		)
 			result = gracely.server.databaseFailure("item not found")
 		else {
+			console.log("inside dequeue", index)
 			console.log("execute callback", item)
-			console.log(index)
-			result = await this.send(item)
-			await this.state.storage.delete(`hook${index}`) // TODO: or reschedule alarm
+			result = await this.send(item.hook)
+			await this.state.storage.delete(`hook${index}`)
 			await this.state.storage.put("index", --index)
-			if (index >= 0)
-				this.state.storage.setAlarm(
-					isoly.DateTime.epoch(isoly.DateTime.nextSecond(isoly.DateTime.now(), 10), "milliseconds")
-				)
+			if (gracely.Error.is(result) && item.retries < this.MAX_RETRIES)
+				this.enqueue(item.hook, ++item.retries)
+			else {
+				if (index >= 0)
+					this.state.storage.setAlarm(
+						isoly.DateTime.epoch(isoly.DateTime.nextSecond(isoly.DateTime.now(), 10), "milliseconds")
+					)
+			}
 		}
 		return result
 	}
@@ -47,10 +50,11 @@ export class Context {
 	async send(item: Item) {
 		const response = await http.fetch({
 			method: "POST",
-			url: "https://ptsv2.com/t/8vc2v-1666168541/post",
+			url: "https://google.com",
 			body: item,
 			header: { contentType: "application/json;charset=UTF-8" },
 		})
+		console.log("inside send", response.status)
 		return response.status < 400 ? item : gracely.server.unavailable("request unsuccessful")
 	}
 }
